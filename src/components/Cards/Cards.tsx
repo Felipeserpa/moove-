dd3a0c7fbbcdc0fecae14b866b430e6658a5a834
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-03-06 11:08:55 -0300

    implemantaçao das fotos

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -11,7 +11,16 @@ import imagem2 from '../../img/moove2.jpg';
 import imagem3 from '../../img/moove3.jpg';
 import imagem4 from '../../img/moove4.jpg';
 import imagem5 from '../../img/moove5.jpg';
+import imagem6 from '../../img/moove6.jpg';
+import { useState } from 'react';
+
 export default function Cards() {
+  const [imagemTelaCheia, setImagemTelaCheia] = useState(null);
+
+  const toggleTelaCheia = (url: string) => {
+    setImagemTelaCheia(imagemTelaCheia === url ? null : url);
+  };
+
   return (
     <div style={{ position: 'relative' }}>
       <Swiper
@@ -30,6 +39,7 @@ export default function Cards() {
             src={imagem1}
             alt="Imagem 1"
             style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+            onClick={() => toggleTelaCheia(imagem1)}
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -37,6 +47,7 @@ export default function Cards() {
             src={imagem2}
             alt="Imagem 2"
             style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+            onClick={() => toggleTelaCheia(imagem2)}
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -46,9 +57,9 @@ export default function Cards() {
             style={{
               width: '100%',
               height: '400px',
-
               objectFit: 'cover',
             }}
+            onClick={() => toggleTelaCheia(imagem3)}
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -56,6 +67,7 @@ export default function Cards() {
             src={imagem4}
             alt="Imagem 4"
             style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+            onClick={() => toggleTelaCheia(imagem4)}
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -63,9 +75,47 @@ export default function Cards() {
             src={imagem5}
             alt="Imagem 5"
             style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+            onClick={() => toggleTelaCheia(imagem5)}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={imagem6}
+            alt="Imagem 5"
+            style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+            onClick={() => toggleTelaCheia(imagem6)}
           />
         </SwiperSlide>
       </Swiper>
+
+      {imagemTelaCheia && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+            cursor: 'pointer',
+          }}
+          onClick={() => toggleTelaCheia(null)}
+        >
+          <img
+            src={imagemTelaCheia}
+            alt="Imagem em Tela Cheia"
+            style={{
+              maxWidth: '40%',
+              maxHeight: '95%',
+              objectFit: 'contain',
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }

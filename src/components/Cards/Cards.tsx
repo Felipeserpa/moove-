d6ffef85ed44a93e4258e73a0d6843544cb5a335
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-03-06 09:52:37 -0300

    implementação dos card

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,9 +1,59 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import imagem1 from '../../img/foto1.jpg';
+import imagem2 from '../../img/foto1.jpg';
+import imagem3 from '../../img/foto1.jpg';
+import imagem4 from '../../img/foto1.jpg';
+
 export default function Cards() {
   return (
-    <div className="image-container">
-      <img src="" alt="Barbeiro" id="img1" />
-
-      <img src="" alt="quantico sinalizacao " id="img" />
+    <div style={{ position: 'relative' }}>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <img
+            src={imagem1}
+            alt="Imagem 1"
+            style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={imagem2}
+            alt="Imagem 2"
+            style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={imagem3}
+            alt="Imagem 3"
+            style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={imagem4}
+            alt="Imagem 4"
+            style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
